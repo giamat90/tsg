f572723cb241d5a2f00c2199e5773fc3766d620d
GiaMat90 <giacominomatzeu@gmail.com>
2024-10-15 21:33:30 +0200

    Fixed include guards in io.h and exaple for benchmark

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include "test/benchmark.h"
 #include "io/io.h"
 
+using tsg::io::printf;
+using tsg::test::do_benchmark;
+
+void foo(int x){
+    for(int i = 0; i < x; ++i);
+}
+
 int main(){
-    tsg::io::printf("Hello { World {{{}}} {}", 616, "Goodbye");
+    printf("Hello World {}", 616);
+
+    auto time = do_benchmark<int>(foo, 100000000000);
+    printf("Time elapsed = {}s", time);
+
+    printf("Goodbye");
     return 0;
-} 
+}
