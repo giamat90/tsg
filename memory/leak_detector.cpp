351fc02a2158891df661cef39133cc07516f446d
Giacomo Matzeu <giacomo.matzeu@medtronic.com>
2024-10-22 18:59:02 +0200

    Featured leak_detector

--- memory/leak_detector.cpp
+++ memory/leak_detector.cpp
@@ -1,15 +1,15 @@
 #include "leak_detector.h"
 
+#include "../io/io.h"
 #include <cstdlib>  // malloc, free
 #include <new>      // new and delete operators, std::bad_alloc
-#include "../io/io.h"  // printf
 
 using tsg::io::printf;
 
 void* operator new(std::size_t size){
     void* ptr = std::malloc(size);
     if(ptr){
-        tsg::leak_detector::get_leak_detector().allocate(size);
+        tsg::leak_detector::get_leak_detector().allocate(ptr, size);
         return ptr;
     } else {
         throw std::bad_alloc();
@@ -19,7 +19,7 @@ void* operator new(std::size_t size){
 void* operator new[](std::size_t size){
     void* ptr = std::malloc(size);
     if(ptr){
-        tsg::leak_detector::get_leak_detector().allocate(size);
+        tsg::leak_detector::get_leak_detector().allocate(ptr, size);
         return ptr;
     } else {
         throw std::bad_alloc();
@@ -28,20 +28,44 @@ void* operator new[](std::size_t size){
 
 void operator delete(void* ptr, std::size_t size){
     if(ptr){
-        tsg::leak_detector::get_leak_detector().tsg::leak_detector::get_leak_detector().deallocate(size);
+        tsg::leak_detector::get_leak_detector().deallocate(ptr, size);
+        std::free(ptr);
+    }
+}
+
+void operator delete(void* ptr){
+    if(ptr){
+        tsg::leak_detector::get_leak_detector().deallocate(ptr);
         std::free(ptr);
     }
 }
 
 void operator delete[](void* ptr, std::size_t size){
     if(ptr){
-        tsg::leak_detector::get_leak_detector().deallocate(size);
+        tsg::leak_detector::get_leak_detector().deallocate(ptr, size);
         std::free(ptr);
     }
 }
 
+void operator delete[](void* ptr){
+    if(ptr){
+        tsg::leak_detector::get_leak_detector().deallocate(ptr);
+        std::free(ptr);
+    }
+}
+
+
 tsg::leak_detector::~leak_detector(){    
-    printf("Memory allocated = {}", m_allocated_bytes);
-    printf("Memory deallocated = {}", m_deallocated_bytes);
-    printf("Memory leaked = {}", m_allocated_bytes - m_deallocated_bytes);
+    printf("Memory allocated = {} bytes\n", m_allocated_bytes);
+    printf("Num allocations = {}\n", m_num_allocation);
+    printf("Memory deallocated = {} bytes\n", m_deallocated_bytes);
+    printf("Num deallocations = {}\n", m_num_deallocation);
+    printf("Memory leaked = {} bytes\n", m_allocated_bytes - m_deallocated_bytes);
+    printf("Memory leaked = {} times\n", m_num_allocation - m_num_deallocation);
+    for(std::size_t i = 0u; i < m_list_size; ++i){
+        if(m_allocation_list[i]){
+            printf("Address leaked = {}\n", m_allocation_list[i]);
+        }
+    }
+
 }
--- memory/leak_detector.h
+++ memory/leak_detector.h
@@ -1,17 +1,55 @@
 #pragma one
 
 #include <cstddef> 
+#include <array>
+#include <algorithm>
 
-namespace tsg{
-    class leak_detector {
+namespace tsg{    class leak_detector {
     public:
-    ~leak_detector();
-    static leak_detector& get_leak_detector(){
-        static leak_detector detector;
-        return detector;    
-    }
-        void allocate(std::size_t bytes){ m_allocated_bytes += bytes; };
-        void deallocate(std::size_t bytes){m_deallocated_bytes += bytes; };
+        ~leak_detector();
+        static leak_detector& get_leak_detector(){
+            static leak_detector detector;
+            return detector;    
+        };
+        void allocate(std::size_t bytes){ 
+            m_allocated_bytes += bytes; 
+            ++m_num_allocation;
+        };
+        void allocate(void* ptr, std::size_t bytes){
+            m_allocation_list[m_allocation_index++] = static_cast<int*>(ptr);
+            m_allocated_bytes += bytes; 
+            ++m_num_allocation;
+        };
+        void deallocate(void* ptr, std::size_t bytes){
+            std::size_t index{};
+            for(index = 0u; index < m_list_size; ++index){
+                if(m_allocation_list[index] == ptr){
+                    break;
+                }
+            }
+            if(index < m_list_size){
+                m_allocation_list[index] = nullptr;
+                m_deallocated_bytes += bytes;
+                ++m_num_deallocation;
+            } 
+        };
+        void deallocate(std::size_t bytes){
+            m_deallocated_bytes += bytes;
+            ++m_num_deallocation;
+        };
+        void deallocate(void* ptr){
+            std::size_t index{};
+            for(index = 0u; index < m_list_size; ++index){
+                if(m_allocation_list[index] == ptr){
+                    break;
+                }
+            }
+            if(index < m_list_size){
+                m_allocation_list[index] = nullptr;
+                ++m_num_deallocation;
+            } 
+        };
+        void deallocate(){ ++m_num_deallocation; };
         std::size_t get_allocated_bytes() const { return m_allocated_bytes; }
         std::size_t get_deallocated_bytes() const { return m_deallocated_bytes; }
         bool is_memory_leaked() { return m_allocated_bytes > m_deallocated_bytes; }
@@ -19,5 +57,11 @@ namespace tsg{
     private:
         std::size_t m_allocated_bytes{};
         std::size_t m_deallocated_bytes{};
+        std::size_t m_num_allocation{};
+        std::size_t m_num_deallocation{};
+        const std::size_t m_list_size{0xffffff};
+        void* m_allocation_list[0xffffff];
+        char* m_allocation_list_string[0xffffff];
+        std::size_t m_allocation_index{};
     };
 }
