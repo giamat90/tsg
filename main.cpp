ba0fa281dd51e11f082d45bf02c7cc28e8e8ca2d
Giacomo Matzeu <giacomo.matzeu@medtronic.com>
2024-10-23 14:09:34 +0200

    WIP: leak_detector with stack trace info

--- main.cpp
+++ main.cpp
@@ -1,19 +1,61 @@
-#include "test/benchmark.h"
 #include "io/io.h"
+// #include "memory/leak_detector.h"
+
+#include <new>
+#include <cstdlib>
+
+#include <vector>
 
 using tsg::io::printf;
-using tsg::test::get_execution_time;
 
-void foo(int x){
-    for(int i = 0; i < x; ++i);
+
+// void* operator new(std::size_t size){
+//     void* ptr = malloc(size);
+//     printf("Allocating {}bytes of memory on address {}", size, ptr);
+//     return ptr;
+// }
+
+// void operator delete(void* ptr, std::size_t size){
+//     printf("Deallocating memory in address {} for bytes {}", ptr, size);
+//     free(ptr);
+// }
+
+// void* operator new[](std::size_t size){
+//     void* ptr = malloc(size);
+//     printf("Allocating array for {}bytes on address {}", size, ptr);
+//     return ptr;
+// }
+
+// void operator delete[](void* ptr){
+//     printf("Deallocating array in address {}", ptr);
+//     free(ptr);
+// }
+
+void consume_memory(){
+    try{
+        std::vector<int> vec;
+        for(;;){
+            vec.push_back(42);
+        }        
+    } catch(std::exception& e){
+        printf("Exception catched {}", e.what());
+    }
 }
 
 int main(){
-    printf("Hello World {}", 616);
+    printf("Hello World\n");
 
-    auto time = get_execution_time<int>(foo, 100000000000);
-    printf("Time elapsed = {}s", time);
+    int* p = new int{};
+    int* q = new int{};
+    int* r = new int{};
 
-    printf("Goodbye");
+
+    printf("p = {}\n", p);
+    printf("q = {}\n", q);
+    printf("r = {}\n", r);
+
+    delete r;
+
+    printf("Goodbye\n");
     return 0;
 }
--- memory/leak_detector.cpp
+++ memory/leak_detector.cpp
@@ -4,10 +4,19 @@
 #include <cstdlib>  // malloc, free
 #include <new>      // new and delete operators, std::bad_alloc
 
+#include <stacktrace>
+// #include <execinfo.h>
 using tsg::io::printf;
 
-void* operator new(std::size_t size){
+void* operator new(std::size_t size){    
+    // void* array[10];
+    // int size_stack = backtrace(array, 10);
+    // char** strings = backtrace_symbols(array, size_stack);
+    // for(int i = 0; i < size_stack; ++i){
+    //     printf("{}\n", strings[i]);
+    // }
     void* ptr = std::malloc(size);
+    tsg::leak_detector::get_leak_detector().addStackInfo(ptr, std::to_string(std::stacktrace::current()));
     if(ptr){
         tsg::leak_detector::get_leak_detector().allocate(ptr, size);
         return ptr;
@@ -16,8 +25,9 @@ void* operator new(std::size_t size){
     }
 }
 
-void* operator new[](std::size_t size){
+void* operator new[](std::size_t size, const char* file, int line){
     void* ptr = std::malloc(size);
+    tsg::leak_detector::get_leak_detector().addStackInfo(ptr, std::to_string(std::stacktrace::current()));
     if(ptr){
         tsg::leak_detector::get_leak_detector().allocate(ptr, size);
         return ptr;
@@ -63,8 +73,11 @@ tsg::leak_detector::~leak_detector(){
     printf("Memory leaked = {} bytes\n", m_allocated_bytes - m_deallocated_bytes);
     printf("Memory leaked = {} times\n", m_num_allocation - m_num_deallocation);
     for(std::size_t i = 0u; i < m_list_size; ++i){
-        if(m_allocation_list[i]){
-            printf("Address leaked = {}\n", m_allocation_list[i]);
+        // if(m_allocation_list[i]){
+        if(m_stack_info_list[i].pointer){
+            // printf("Address leaked = {}\n", m_allocation_list[i]);
+            printf("Address leaked = {}\n", m_stack_info_list[i].pointer);
+            printf("Stack info = {}\n", m_stack_info_list[i].stack_description);
         }
     }
 
