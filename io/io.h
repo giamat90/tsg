264645687e941377ed89be9a5197e39169f513ac
GiaMat90 <giacominomatzeu@gmail.com>
2024-10-29 22:43:23 +0100

    Refactor: rename printf as print

--- io/io.h
+++ io/io.h
@@ -6,16 +6,16 @@ namespace tsg
 {
     namespace io {
         template <typename T>
-        void printf(const T& value){
+        void print(const T& value){
             std::cout << value;
         }
 
         template <typename T, typename ...Args>
-        void printf(const char* str, const T& value, Args... args){
+        void print(const char* str, const T& value, Args... args){
             while(str && *str){
                 if(('{' == *str) && ('}' == *(str + 1))){
                     std::cout << value;
-                    return printf(++(++str), args...);
+                    return print(++(++str), args...);
                 } else if('\n' == *str) {
                     std::cout << std::endl;
                 } else {
