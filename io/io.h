87002c86bfc99c4cadd61c8a241865090ca033af
GiaMat90 <giacominomatzeu@gmail.com>
2024-11-10 13:27:09 +0100

    Remove nested namespace in io.h

--- io/io.h
+++ io/io.h
@@ -4,24 +4,26 @@
 
 namespace tsg
 {
-    namespace io {
-        template <typename T>
-        void print(const T& value){
-            std::cout << value;
-        }
+    template <typename T>
+    void print(const T& value){
+        std::cout << value << std::endl;
+    }
 
-        template <typename T, typename ...Args>
-        void print(const char* str, const T& value, Args... args){
-            while(str && *str){
-                if(('{' == *str) && ('}' == *(str + 1))){
-                    std::cout << value;
-                    return print(++(++str), args...);
-                } else if('\n' == *str) {
-                    std::cout << std::endl;
-                } else {
-                    std::cout << *str++;
-                }
+    template <typename T, typename ...Args>
+    void print(const char* str, const T& value, Args... args){
+        while(str && *str){
+            if(('{' == *str) && ('}' == *(str + 1))){
+                std::cout << value;
+                return print(++(++str), args...);
+            } else if('\n' == *str) {
+                std::cout << std::endl;
+            } else {
+                std::cout << *str++;
             }
         }
+    }
+
+    void new_line(){
+        std::cout << std::endl;
     }
 }
