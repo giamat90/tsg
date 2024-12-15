9b10e54a028e519f757f44c12df47b1892911acf
GiaMat90 <giacominomatzeu@gmail.com>
2024-12-15 11:57:35 +0100

    Add object class in types.h

--- types.h
+++ types.h
@@ -11,6 +11,12 @@ namespace tsg {
         non_copyable& operator=(const non_copyable&) = delete;
     };
 
+    class object {
+    public:
+        object() = default;
+        ~object() = default;
+    }
+
     template <typename T, typename = std::enable_if_t<std::is_same<T, int>::value>>
     static constexpr bool is_ice(T v) {
         return true;
