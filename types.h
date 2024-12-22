60939a39184459a68fab8e0c26c2a681e2fd40da
GiaMat90 <giacominomatzeu@gmail.com>
2024-12-22 19:19:54 +0100

    Fix one definition rule bug for io.h. WIP: iterator_lesson

--- types.h
+++ types.h
@@ -15,7 +15,7 @@ namespace tsg {
     public:
         object() = default;
         ~object() = default;
-    }
+    };
 
     template <typename T, typename = std::enable_if_t<std::is_same<T, int>::value>>
     static constexpr bool is_ice(T v) {
